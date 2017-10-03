715a661301d388367201c7661a2135c2f7e10edd
Zackys <toshy.miyazaki@gmail.com>
2017-10-03 15:46:08 +0900

    improve

--- sample01/src/main/java/zackys/neo4j/handson01/mail/app/App08_MailLogging.java
+++ sample01/src/main/java/zackys/neo4j/handson01/mail/app/App08_MailLogging.java
@@ -1,18 +1,13 @@
 package zackys.neo4j.handson01.mail.app;
 
 import java.util.ArrayDeque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
 
 import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.transaction.Transaction;
 
 import zackys.neo4j.handson01.mail.model.Email;
 import zackys.neo4j.handson01.mail.model.MailData;
-import zackys.neo4j.handson01.mail.model.User;
 import zackys.neo4j.handson01.mail.neo4j.ogm.session.Neo4jSessionFactory;
 
 /**
@@ -30,7 +25,8 @@ public class App08_MailLogging {
 
         try (Transaction tx = session.beginTransaction()) {
 
-            session.deleteAll(Email.class);
+            // :Emailを全部消してやり直したい場合
+            //session.deleteAll(Email.class);
 
             Queue<MailData> queue = new ArrayDeque<MailData>() {
                 {
@@ -41,27 +37,8 @@ public class App08_MailLogging {
                 }
             };
 
-            Map<String, String> param = new HashMap<>();
-            queue.stream().forEach(data->{
-                param.clear();
-                param.put("addr", data.from);
-                User from = session.queryForObject(User.class, "MATCH (n:User {address:{addr}}) RETURN n", param);
-
-                Set<User> res = new HashSet<>();
-                for (String toAddr: data.to) {
-                    param.clear();
-                    param.put("addr", toAddr);
-                    User to = session.queryForObject(User.class, "MATCH (n:User {address:{addr}}) RETURN n", param);
-
-                    res.add(to);
-                }
-
-                long cnt = session.countEntitiesOfType(Email.class);
-
-                Email mail = new Email(cnt+1, data.title, from, res);
-
-                session.save(mail);
-            });
+            // :Emailの要素数を返す。今回はこれに1加えた値をmailIdとして登録する。
+            long cnt = session.countEntitiesOfType(Email.class);
 
             tx.commit();
         }
